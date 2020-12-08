38dba1a4b226da25a7e80b32af2a742550e43aea
brooke <daisyrakionvideos@gmail.com>
2020-12-07 20:28:46 -0600

    added goToPreviousBook function and button onClick for next book function.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -13,20 +13,42 @@ class App extends Component{
             bookNumber:0
         }
     }
+
+    goToNextBook(){
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length{
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook(){
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0)
+            tempBookNumber = this.books.length -1;
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
     render = (){
         return (
            <div className="container-fluid">
                 <TitleBar />
                 <div className="row row-spacer">
-                <div className="col">
+                <div className="col-md-4">
                 {/*Button here to move to the previous book viewed*/}
              </div>
             <div className ="col-md-4">
                 <h1>{this.books[this.state.bookNumber].title}</h1>
                 <h4>{this.books[this.state.bookNumber].author</h4>
                         </div>
-            <div className="col">
-            {/*Button here to move to the previous book viewed*/}
+            <div className="col-md-4">
+            <button onClick={() => this.goToNextBook()}>Next Book</button>
             </div>
         </div>
     </div>
