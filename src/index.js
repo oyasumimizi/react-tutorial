e1a03e02f3414558b738fdc2a11b49d85d74bf79
brooke <daisyrakionvideos@gmail.com>
2020-12-07 19:35:45 -0600

    added app.css with some code, added two new imports to index.js.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/app";
+import "bootstrap/dist/css/bootstrap.css”;
+
 
 const jsxElement = <h1>Our React App</h1>;
 console.log(jsxElement);
