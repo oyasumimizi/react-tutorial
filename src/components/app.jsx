dc15fc671ee96c0a7cdb6b43e687fc444ac0ba17
brooke <daisyrakionvideos@gmail.com>
2020-12-07 19:25:19 -0600

    Added app.jsx, started the component code from tutorial.

---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class App extends Component{
+    state = { }
+    render = (){
+        return(
+            <h1>Our React App Using a Component</h1>
+        );
+    }
+}
