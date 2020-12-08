23d902e911fe3e232c12f032f55f61521f12cecd
brooke <daisyrakionvideos@gmail.com>
2020-12-07 21:20:32 -0600

    added more code to render method of bookcreator, added handlechange and handlesubmit methods.

---
+++ src/components/BookCreator/bookCreator.jsx
@@ -0,0 +1,52 @@
+const { Component } = require("react")
+
+import React {Component} from "react";
+
+class BookCreator extends Component{
+    constructor(props){
+        super(props);
+        this.state={ }
+            title= "",
+            author= ""
+    }
+
+handleChange(event){
+    this.setState({
+        [event.target.name]: event.target.value
+    });
+}
+
+handleSubmit(event){
+    event.preventDefault();
+    console.log(this.state.title);
+    console.log(this.state.author);
+}
+
+    render(){
+        return(
+            <div>
+               <form onSubmit={this.handleSubmit}>
+                   <div className="row col-allign">
+                       <label>Title:</label>
+                       <input type ="text" name="title" value={this.state.title}
+                       onChange={this.handleChange}/>
+                   </div>
+                   <div className="col-md-4">
+                       <label>Author:</label>
+                       <input type="text" name="title" value={this.state.author}
+                       onChange={this.handleChange}/>
+                   </div>
+                   <div className="cold-md-4">
+                       <input type="submit" value="Add"/>
+                   </div>
+                <div>
+            </form>
+        </div>
+    );
+}
+
+
+
+
+
+export default BookCreator;
