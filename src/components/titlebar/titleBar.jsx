89c290f6ea9c9ee22a2b48f1521f5a85b3e2d5db
brooke <daisyrakionvideos@gmail.com>
2020-12-07 19:42:45 -0600

    added titlebar component with subfolders, added code into titleBar jsx and css.

---
+++ src/components/titlebar/titleBar.jsx
@@ -0,0 +1,15 @@
+import React from "react”;
+
+function TitleBar(props){
+    return(
+        <div className="row row-spacer">
+            <div className="col-md-12" style={{padding: 0}}>
+                <div>
+                    <h1>Brooke's Library</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TitleBar;
